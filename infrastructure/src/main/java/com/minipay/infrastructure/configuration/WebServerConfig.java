8bae061d595a4acea0fae2e3d0af44c53b0e0e4e
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2024-05-26 16:03:11 -0300

    feat: add infra and deps

---
+++ infrastructure/src/main/java/com/minipay/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.minipay.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.minipay")
+public class WebServerConfig {
+}
